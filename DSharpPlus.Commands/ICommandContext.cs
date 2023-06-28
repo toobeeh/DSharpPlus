32dc092b4b4dee991247c1fbf89f645922d32450
instellate <thecrazyrobots@gmail.com>
2023-06-28 21:45:03 +0200

    Made a classlib called DSharpPlus.Commands

---
+++ DSharpPlus.Commands/ICommandContext.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.Entities;
+
+namespace DSharpPlus.Commands;
+
+/// <summary>
+/// A interface for command context
+/// </summary>
+/// <remarks>
+/// Only one property should be null between interaction and message.
+/// </remarks>
+public interface ICommandContext
+{
+    public DiscordInteraction? Interaction { get; set; }
+    public DiscordMessage? Message { get; set; }
+
+    public CommandData CommandData { get; set; }
+}
