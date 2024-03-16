4dc6a0dbce778756fe0eab71de0664305b366c70
akiraveliara <akiraveliara@outlook.com>
2024-03-16 17:51:46 +0100

    reduce the slot table to single bytes

--- DSharpPlus/Net/Rest/Ratelimiting/RatelimitBucketContainer.cs
+++ DSharpPlus/Net/Rest/Ratelimiting/RatelimitBucketContainer.cs
@@ -7,39 +7,37 @@ namespace DSharpPlus.Net.Ratelimiting;
 
 internal sealed class RatelimitBucketContainer
 {
-    private SlotTableEntry[] slotTable = new SlotTableEntry[16];
+    private Slot[] slotTable = new Slot[16];
     private Entry[] entries = new Entry[16];
     private int currentLength = 16;
 
     private SpinLock resizeLock = new();
     private bool isResizing = false;
 
-    public RatelimitBucketLease LeaseOrCreateRouteAsync(string route)
+    public RatelimitBucketLease? LeaseOrCreateRouteAsync(string route)
     {
         Debug.Assert(this.slotTable.Length == this.entries.Length);
         SpinWait.SpinUntil(() => !this.isResizing);
 
         for (int i = 0; i < this.slotTable.Length; i++)
         {
-            scoped ref SlotTableEntry slot = ref this.slotTable[i];
+            scoped ref Slot slot = ref this.slotTable[i];
 
-            if (!slot.isLive)
+            if (!slot.HasFlag(Slot.Live))
             {
                 continue;
             }
 
-            slot.rwlock.EnterReadLock();
+            SpinWait.SpinUntil(() => !this.slotTable[i].HasFlag(Slot.Locked));
+            slot |= Slot.Locked;
 
-            if (this.entries[i].routes.Contains(route))
+            if (this.entries[i].routes.Contains(route) && this.entries[i].bucket.CheckNextRequest())
             {
-                slot.rwlock.ExitReadLock();
-                slot.rwlock.EnterWriteLock();
-                slot.isLeased = true;
-
+                slot ^= Slot.Locked;
                 return new(this, i);
             }
 
-            slot.rwlock.ExitReadLock();
+            slot ^= Slot.Locked;
             continue;
         }
 
@@ -49,20 +47,23 @@ internal sealed class RatelimitBucketContainer
 
         for (int i = 0; i < this.slotTable.Length; i++)
         {
-            scoped ref SlotTableEntry slot = ref this.slotTable[i];
+            scoped ref Slot slot = ref this.slotTable[i];
 
-            if (slot.isLive)
+            if (slot.HasFlag(Slot.Live))
             {
                 continue;
             }
 
-            slot.rwlock.EnterWriteLock();
+            SpinWait.SpinUntil(() => !this.slotTable[i].HasFlag(Slot.Locked));
+            slot |= Slot.Locked;
 
+            slot |= Slot.Live;
             scoped ref Entry entry = ref this.entries[i];
             entry.bucket = new();
             entry.hash = "pending";
             entry.routes = [route];
 
+            slot ^= Slot.Locked;
             return new(this, i);
         }
 
@@ -88,7 +89,7 @@ internal sealed class RatelimitBucketContainer
 
     private void ResizeBackingStorage()
     {
-        SlotTableEntry[] newSlotTable = new SlotTableEntry[this.currentLength * 2];
+        Slot[] newSlotTable = new Slot[this.currentLength * 2];
         Entry[] newEntries = new Entry[this.currentLength * 2];
 
         this.currentLength *= 2;
@@ -102,16 +103,11 @@ internal sealed class RatelimitBucketContainer
         this.isResizing = false;
     }
 
-    // absolute horrendous garbage, but we need to get this fix out one of these days. we can improve this later
-    private struct SlotTableEntry
+    [Flags]
+    private enum Slot : byte
     {
-        public ReaderWriterLockSlim rwlock = new();
-        public bool isLive;
-        public bool isLeased;
-
-        public SlotTableEntry()
-        {
-        }
+        Locked = 1 << 1,
+        Live = 1 << 2,
     }
 
     private struct Entry
